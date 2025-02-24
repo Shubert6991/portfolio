e9fcf4480e499de520df9efaf1e4b5887f27b6ee
Shubert Alonzo <alonzoshubert6991@gmail.com>
2025-02-23 20:28:17 -0600

    integration with chakra ui

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import { Container, Box } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box borderRadius="lg" bg="red" p={3} mb={6} align="center">
+        Hello, I'm a full-stack developer based in Guatemala!
+      </Box>
+    </Container>
+  )
+}
+
+export default Page
